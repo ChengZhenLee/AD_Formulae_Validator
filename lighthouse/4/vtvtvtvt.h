10584dab34e7cc52cfc293bb5a294aea5a947e3a
ChengZhenLee <zhensstories@gmail.com>
2026-04-13 15:55:55 +0200

    Made progress on vtvtvtvt

--- lighthouse/4/vtvtvtvt.h
+++ lighthouse/4/vtvtvtvt.h
@@ -55,7 +55,7 @@ void AD_F_xxxx(
   Eigen::Matrix<Eigen::Matrix<T, V3, V3>, n, V1>& x_1_3_4,
   Eigen::Matrix<Eigen::Matrix<T, V3, V4>, n, V2>& x_2_3_4,
   Eigen::Vector<Eigen::Matrix<Eigen::Matrix<T, V3, V4>, V1, V2>, n>& x_1_2_3_4,
-  const Y_t<T>& y_values,
+  Y_t<T>& y_values,
   Eigen::Matrix<T, m, V1>& y_1, Eigen::Matrix<T, m, V2>& y_2, Eigen::Matrix<T, m, V3>& y_3, Eigen::Matrix<T, m, V4>& y_4,
   Eigen::Vector<Eigen::Matrix<T, V1, V2>, m>& y_1_2, Eigen::Vector<Eigen::Matrix<T, V1, V3>, m>& y_1_3, Eigen::Vector<Eigen::Matrix<T, V1, V4>, m>& y_1_4,
   Eigen::Vector<Eigen::Matrix<T, V2, V3>, m>& y_2_3, Eigen::Vector<Eigen::Matrix<T, V2, V4>, m>& y_2_4,
@@ -154,20 +154,200 @@ void AD_F_xxxx(
   // Extract y
   for (int j = 0; j < m; j++) {
     y_values(j) = y(j).value().value().value().value();
+
+    // Extract Y^{(1)}
+    for (int v1 = 0; v1 < V1; v1++) {
+      y_1(j, v1) = y(j).tangent(v1).value().value().value();
+
+      // Extract Y^{(1, 2)}
+      for (int v2 = 0; v2 < V2; v2++) {
+        y_1_2(j)(v1, v2) = y(j).tangent(v1).tangent(v2).value().value();
+
+        // Extract Y^{(1, 2, 3)}
+        for (int v3 = 0; v3 < V3; v3++) {
+          y_1_2_3(j, v1)(v2, v3) = y(j).tangent(v1).tangent(v2).tangent(v3).value();
+
+          // Extract Y^{(1, 2, 3, 4)}
+          for (int v4 = 0; v4 < V4; v4++) {
+            y_1_2_3_4(j)(v1, v2)(v3, v4) = y(j).tangent(v1).tangent(v2).tangent(v3).tangent(v4);
+          }
+        }
+
+        // Extract Y^{(1, 2, 4)}
+        for (int v4 = 0; v4 < V4; v4++) {
+          y_1_2_4(j, v1)(v2, v4) = y(j).tangent(v1).tangent(v2).value().tangent(v4);
+        }
+      }
+
+      // Extract Y^{(1, 3)}
+      for (int v3 = 0; v3 < V3; v3++) {
+        y_1_3(j)(v1, v3) = y(j).tangent(v1).value().tangent(v3).value();
+
+        // Extract Y^{(1, 3, 4)}
+        for (int v4 = 0; v4 < V4; v4++) {
+          y_1_3_4(j, v1)(v3, v4) = y(j).tangent(v1).value().tangent(v3).tangent(v4);
+        }
+      }
+
+      // Extract Y^{(1, 4)}
+      for (int v4 = 0; v4 < V4; v4++) {
+        y_1_4(j)(v1, v4) = y(j).tangent(v1).value().value().tangent(v4);
+      }
+    }
+
+    // Extract Y^{(2)}
+    for (int v2 = 0; v2 < V2; v2++) {
+      y_2(j, v2) = y(j).value().tangent(v2).value().value();
+
+      // Extract Y^{(2, 3)}
+      for (int v3 = 0; v3 < V3; v3++) {
+        y_2_3(j)(v2, v3) = y(j).value().tangent(v2).tangent(v3).value();
+
+        // Extract Y^{(2, 3, 4)}
+        for (int v4 = 0; v4 < V4; v4++) {
+          y_2_3_4(j, v2)(v3, v4) = y(j).value().tangent(v2).tangent(v3).tangent(v4);
+        }
+      }
+      // Extract Y^{(2, 4)}
+      for (int v4 = 0; v4 < V4; v4++) {
+        y_2_4(j)(v2, v4) = y(j).value().tangent(v2).value().tangent(v4);
+      }
+    }
+
+    // Extract Y^{(3)}
+        for (int v3 = 0; v3 < V3; v3++) {
+      y_3(j, v3) = y(j).value().value().tangent(v3).value();
+
+      // Extract Y^{(3, 4)}
+      for (int v4 = 0; v4 < V4; v4++) {
+        y_3_4(j)(v3, v4) = y(j).value().value().tangent(v3).tangent(v4);
+      }
+    }
+
+    // Extract Y^{(4)}
+        for (int v4 = 0; v4 < V4; v4++) {
+      y_4(j, v4) = y(j).value().value().value().tangent(v4);
+    }
   }
-  // Extract Y^{(1)}
-  // Extract Y^{(2)}
-  // Extract Y^{(3)}
-  // Extract Y^{(4)}
-  // Extract Y^{(1, 2)}
-  // Extract Y^{(1, 3)}
-  // Extract Y^{(1, 4)}
-  // Extract Y^{(2, 3)}
-  // Extract Y^{(2, 4)}
-  // Extract Y^{(3, 4)}
-  // Extract Y^{(1, 2, 3)}
-  // Extract Y^{(1, 2, 4)}
-  // Extract Y^{(1, 3, 4)}
-  // Extract Y^{(2, 3, 4)}
-  // Extract Y^{(1, 2, 3, 4)}
+}
+
+
+template<typename T, int V1, int V2, int V3, int V4>
+void Formula_F_xxxx(
+  Y_X_t<T>& y_x, Y_XX_t<T>& y_xx, Y_XXX_t<T>& y_xxx, Y_XXXX_t<T>& y_xxxx,
+  const X_t<T>& x_values,
+  Eigen::Matrix<T, n, V1>& x_1, Eigen::Matrix<T, n, V2>& x_2, Eigen::Matrix<T, n, V3>& x_3, Eigen::Matrix<T, n, V4>& x_4, 
+  Eigen::Vector<Eigen::Matrix<T, V1, V2>, n>& x_1_2, Eigen::Vector<Eigen::Matrix<T, V1, V3>, n>& x_1_3, Eigen::Vector<Eigen::Matrix<T, V1, V4>, n>& x_1_4, 
+  Eigen::Vector<Eigen::Matrix<T, V2, V3>, n>& x_2_3, Eigen::Vector<Eigen::Matrix<T, V2, V4>, n>& x_2_4, 
+  Eigen::Vector<Eigen::Matrix<T, V3, V4>, n>& x_3_4, 
+  Eigen::Matrix<Eigen::Matrix<T, V2, V3>, n, V1>& x_1_2_3,
+  Eigen::Matrix<Eigen::Matrix<T, V2, V4>, n, V1>& x_1_2_4,
+  Eigen::Matrix<Eigen::Matrix<T, V3, V3>, n, V1>& x_1_3_4,
+  Eigen::Matrix<Eigen::Matrix<T, V3, V4>, n, V2>& x_2_3_4,
+  Eigen::Vector<Eigen::Matrix<Eigen::Matrix<T, V3, V4>, V1, V2>, n>& x_1_2_3_4,
+  Y_t<T>& y_values,
+  Eigen::Matrix<T, m, V1>& y_1, Eigen::Matrix<T, m, V2>& y_2, Eigen::Matrix<T, m, V3>& y_3, Eigen::Matrix<T, m, V4>& y_4,
+  Eigen::Vector<Eigen::Matrix<T, V1, V2>, m>& y_1_2, Eigen::Vector<Eigen::Matrix<T, V1, V3>, m>& y_1_3, Eigen::Vector<Eigen::Matrix<T, V1, V4>, m>& y_1_4,
+  Eigen::Vector<Eigen::Matrix<T, V2, V3>, m>& y_2_3, Eigen::Vector<Eigen::Matrix<T, V2, V4>, m>& y_2_4,
+  Eigen::Vector<Eigen::Matrix<T, V3, V4>, m>& y_3_4,
+  Eigen::Matrix<Eigen::Matrix<T, V2, V3>, m, V1>& y_1_2_3,
+  Eigen::Matrix<Eigen::Matrix<T, V2, V4>, m, V1>& y_1_2_4,
+  Eigen::Matrix<Eigen::Matrix<T, V3, V4>, m, V1>& y_1_3_4,
+  Eigen::Matrix<Eigen::Matrix<T, V3, V4>, m, V2>& y_2_3_4,
+  Eigen::Vector<Eigen::Matrix<Eigen::Matrix<T, V3, V4>, V1, V2>, m>& y_1_2_3_4
+) {
+  // y = f(x)
+  F(x_values, y_values);
+
+  for (int j = 0; j < m; j++) {
+    // Y^{(1)} = F' * X^{(1)}
+    for (int v1 = 0; v1 < V1; v1++ ) {
+      T sum = 0;
+      for (int i1 = 0; i1 < n; i1++) {
+          sum += y_x(j, i1) * x_1(i1, v1);
+      }
+      y_1(j, v1) = sum;
+    }
+
+    // Y^{(2)} = F' * X^{(2)}
+    for (int v2 = 0; v2 < V2; v2++ ) {
+      T sum = 0;
+      for (int i2 = 0; i2 < n; i2++) {
+          sum += y_x(j, i2) * x_2(i2, v2);
+      }
+      y_2(j, v2) = sum;
+    }
+
+    // Y^{(3)} = F' * X^{(3)}
+    for (int v3 = 0; v3 < V3; v3++ ) {
+      T sum = 0;
+      for (int i3 = 0; i3 < n; i3++) {
+          sum += y_x(j, i3) * x_3(i3, v3);
+      }
+      y_3(j, v3) = sum;
+    }
+
+    // Y^{(4)} = F' * X^{(4)}
+    for (int v4 = 0; v4 < V4; v4++ ) {
+      T sum = 0;
+      for (int i4 = 0; i4 < n; i4++) {
+          sum += y_x(j, i4) * x_4(i4, v4);
+      }
+      y_4(j, v4) = sum;
+    }
+  }
+
+  // Y^{(1, 2)} = F'' * X^{(1)} * X^{(2)} + F' * X^{(1, 2)}
+  for (int j = 0; j < m; j++) {
+    for (int v1 = 0; v1 < V1; v1++) {
+      for (int v2 = 0; v2 < V2; v2++) {
+        
+      }
+    }
+  }
+
+  // Y^{(1, 3)} = F'' * X^{(1)} * X^{(3)} + F' * X^{(1, 3)}
+
+  // Y^{(1, 4)} = F'' * X^{(1)} * X^{(4)} + F' * X^{(1, 4)}
+
+  // Y^{(2, 3)} = F'' * X^{(2)} * X^{(3)} + F' * X^{(2, 3)}
+
+  // Y^{(2, 4)} = F'' * X^{(2)} * X^{(4)} + F' * X^{(2, 4)}
+
+  // Y^{(3, 4)} = F'' * X^{(3)} * X^{(4)} + F' * X^{(3, 4)}
+
+  // Y^{(1, 2, 3)} = F''' * X^{(1)} * X^{(2)} * X^{(3)}
+  // + F'' * X^{(2)} * X^{(1, 3)}
+  // + F'' * X^{(1)} * X^{(2, 3)}
+  // + F'' * X^{(3)} * X^{(1, 2)}
+  // + F' * X^{(1, 2, 3)}
+
+  // Y^{(1, 2, 4)} = F''' * X^{(1)} * X^{(2)} * X^{(4)}
+  // + F'' * X^{(2)} * X^{(1, 4)}
+  // + F'' * X^{(1)} * X^{(2, 4)}
+  // + F'' * X^{(4)} * X^{(1, 2)}
+  // + F' * X^{(1, 2, 4)}
+
+  // Y^{(2, 3, 4)} = F''' * X^{(2)} * X^{(3)} * X^{(4)}
+  // + F'' * X^{(2)} * X^{(3, 4)}
+  // + F'' * X^{(3)} * X^{(2, 4)}
+  // + F'' * X^{(4)} * X^{(2, 3)}
+  // + F' * X^{(2, 3, 4)}
+
+  // Y^{(1, 2, 3, 4)}
+  // = F'''' * X^{(1)} * X^{(2)} * X^{(3)} * X^{(4)}
+  // + F''' * X^{(1)} * X^{(2)} * X^{(3, 4)}
+  // + F''' * X^{(1)} * X^{(3)} * X^{(2, 4)}
+  // + F''' * X^{(1)} * X^{(4)} * X^{(2, 3)}
+  // + F''' * X^{(2)} * X^{(3)} * X^{(1, 4)}
+  // + F''' * X^{(2)} * X^{(4)} * X^{(1, 3)}
+  // + F''' * X^{(3)} * X^{(4)} * X^{(1, 4)}
+  // + F'' * X^{(1)} * X^{(2, 3, 4)}
+  // + F'' * X^{(1, 2)} * X^{(3, 4)}
+  // + F'' * X^{(1, 4)} * X^{(2, 3)}
+  // + F'' * X^{(1, 2, 3)} * X^{(4)}
+  // + F'' * X^{(2)} * X^{(1, 3, 4)}
+  // + F'' * X^{(2, 4)} * X^{(1, 3)}
+  // + F'' * X^{(3)} * X^{(1, 2, 4)}
+  // + F' * X^{(1, 2, 3, 4)}
 }
